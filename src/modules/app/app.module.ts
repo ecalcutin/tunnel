763c4f51a836264bf2846218568102d7ee438759
Evgenii Kalkutin <evgheni.calcutin@gmail.com>
2025-05-15 04:46:15 +0300

    feat(): Add config module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { AppConfigModule } from '../config';
 import { WireguardModule } from '../wireguard';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [WireguardModule],
+  imports: [AppConfigModule, WireguardModule],
   providers: [AppService],
   controllers: [AppController],
 })
---
+++ src/modules/config/index.ts
@@ -0,0 +1,2 @@
+export { AppConfigModule } from './config.module';
+export { AppConfigService } from './config.service';
