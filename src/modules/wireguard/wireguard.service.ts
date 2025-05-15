a46f2e4854f2c0d015416db6a42c7fb60392f63c
Evgenii Kalkutin <evgheni.calcutin@gmail.com>
2025-05-15 06:58:35 +0300

    draft(): just draft commit

--- src/modules/wireguard/wireguard.service.ts
+++ src/modules/wireguard/wireguard.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+
+import { AppConfigService } from '../config';
+
+import { generateServerConfig } from './utils/generate-server-config';
+import writeWireguardConfig from './utils/write-wireguard-config';
 
 @Injectable()
-export class WireguardService {}
+export class WireguardService {
+  constructor(
+    @Inject(AppConfigService)
+    private readonly appConfigService: AppConfigService,
+  ) {}
+
+  public async readInterfaces() {
+    return [];
+  }
+
+  public async createInterface() {
+    const serverInterface = this.buildServerConfig();
+    this.saveServerConfig(serverInterface);
+    return serverInterface;
+  }
+
+  private saveServerConfig(config: string): void {
+    writeWireguardConfig(config);
+  }
+
+  private buildServerConfig(): string {
+    const { privateKey, publicKey } = this.appConfigService.WIREGUARD!;
+    const serverConfig = generateServerConfig({
+      privateKey: privateKey,
+      address: '10.0.0.1/24',
+      listenPort: 51820,
+      peers: [
+        {
+          publicKey: publicKey,
+          allowedIPs: '10.0.0.2/32',
+        },
+      ],
+    });
+    return serverConfig;
+  }
+}
