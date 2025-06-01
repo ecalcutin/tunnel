51e8c0622fbc7effe360adfd8788ffdf6f7ff8d0
Evgenii Kalkutin <evgheni.calcutin@gmail.com>
2025-06-01 09:23:19 +0300

    feat(): Inject config

--- src/modules/features/wireguard/wireguard.module.ts
+++ src/modules/features/wireguard/wireguard.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { AppConfigModule } from '../../config';
+
 import { WireguardService } from './wireguard.service';
 
 @Module({
+  imports: [AppConfigModule],
   providers: [WireguardService],
   exports: [WireguardService],
 })
--- src/modules/features/wireguard/wireguard.service.ts
+++ src/modules/features/wireguard/wireguard.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+
+import { AppConfigService } from '../../config';
 
 @Injectable()
-export class WireguardService {}
+export class WireguardService {
+  private readonly privateKey: string;
+  private readonly publicKey: string;
+
+  constructor(
+    @Inject(AppConfigService)
+    private readonly appConfigService: AppConfigService,
+  ) {
+    const wireguardConfig = appConfigService.WIREGUARD;
+    this.privateKey = wireguardConfig.privateKey;
+    this.publicKey = wireguardConfig.publicKey;
+  }
+}
